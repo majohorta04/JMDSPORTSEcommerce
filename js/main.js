4169718c2c1d372f501ec85518e3de249e5aa8a5
majohorta04 <2220211028@estudiantesunibague.edu.co>
2023-05-16 16:52:30 -0500

    nuevos cambios main.js

--- js/main.js
+++ js/main.js
@@ -3,202 +3,202 @@ const productos = [
     //Accesorios
     {
         id: "a1",
-        titulo: "Morral",           
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a1.png",
+        titulo: "Morrali",           
+        imagen: "../img/Mujer/m1.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 150.000 
+        precio: 150000 
     },
     {
         id: "a2",
         titulo: "Maleta",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a2.png",
+        imagen: "../img/Mujer/m2.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 200.000 
+        precio: 200000 
     },
     {
         id: "a3",
         titulo: "Gafas mujer",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a3.png",
+        imagen: "../img/accesorios/a3.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 80.000 
+        precio: 80000 
     },
     {
         id: "a4",
         titulo: "Gafas hombre",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a4.png",
+        imagen: "../img/accesorios/a4.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 90.000 
+        precio: 90000 
     },
     {
         id: "a5",
         titulo: "Maleta",           
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a5.png",
+        imagen: "../img/accesorios/a5.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 180.000 
+        precio: 180000 
     },
     {
         id: "a6",
         titulo: "Reloj hombre",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a6.png",
+        imagen: "../img/accesorios/a6.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 300.000 
+        precio: 300000 
     },
     {
         id: "a7",
         titulo: "Gorra plana",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a7.png",
+        imagen: "../img/accesorios/a7.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 130.000 
+        precio: 130000 
     },
     {
         id: "a8",
         titulo: "Gorra",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a8.png",
+        imagen: "../img/accesorios/a8.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 120.000 
+        precio: 120000 
     },
     {
         id: "a9",
         titulo: "Reloj dama",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\accesorios\a9.png",
+        imagen: "../img/accesorios/a9.png",
         categoria: {
             nombre: "Accesorios",
             id: "accesorios"
         },
-        precio: 250.000 
+        precio: 250000 
     },
 
     //Hombre
     {
         id: "h1",
         titulo: "Camisa",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h1.png",
+        imagen: "../img/Hombre/h1.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 200.000 
+        precio: 200000 
     },
     {
         id: "h2",
         titulo: "Conjunto",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h2.png",
+        imagen: "../img/Hombre/h2.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 400.000 
+        precio: 400000 
     },
     {
         id: "h3",
         titulo: "Camisa",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h3.png",
+        imagen: "../img/Hombre/h3.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 200.000 
+        precio: 200000 
     },
     {
         id: "h4",
         titulo: "Camisa",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h4.png",
+        imagen: "../img/Hombre/h4.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 180.000 
+        precio: 180000 
     },
     {
         id: "h5",
         titulo: "Buso",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h5.png",
+        imagen: "../img/Hombre/h5.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 300.000 
+        precio: 300000 
     },
     {
         id: "h6",
         titulo: "Jogger",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h6.png",
+        imagen: "../img/Hombre/h6.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 230.000 
+        precio: 230000 
     },
     {
         id: "h7",
         titulo: "Sudadera",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h7.png",
+        imagen: "../img/Hombre/h7.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 400.000 
+        precio: 400000 
     },
     {
         id: "h8",
         titulo: "Chaqueta",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h8.png",
+        imagen: "../img/Hombre/h8.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 230.000 
+        precio: 230000 
     },
     {
         id: "h9",
         titulo: "Sudadera",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Hombre\h9.png",
+        imagen: "../img/Hombre/h9.png",
         categoria: {
             nombre: "Hombre",
             id: "hombre"
         },
-        precio: 250.000 
+        precio: 250000 
     },
 
     //Mujer
     {
         id: "m1",
         titulo: "Blusa",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m1.png",
+        imagen: "../img/Mujer/m1.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 95.000 
+        precio: 95000 
     },
     {
         id: "m2",
         titulo: "Conjunto",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m2.png",
+        imagen: "../img/Mujer/m2.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
@@ -208,67 +208,67 @@ const productos = [
     {
         id: "m3",
         titulo: "Leggie",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m3.png",
+        imagen: "../img/Mujer/m3.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 130.000 
+        precio: 130000 
     },
     {
         id: "m4",
         titulo: "Conjunto",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m4.png",
+        imagen: "../img/Mujer/m4.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 430.000 
+        precio: 430000 
     },
     {
         id: "m5",
         titulo: "Chaqueta",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m5.png",
+        imagen: "../img/Mujer/m5.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 390.000 
+        precio: 390000 
     },
     {
         id: "m6",
         titulo: "Camisa rosada",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m6.png",
+        imagen: "../img/Mujer/m6.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 180.000 
+        precio: 180000 
     },
     {
         id: "m7",
         titulo: "Camisa salmon",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m7.png",
+        imagen: "../img/Mujer/m7.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 90.000 
+        precio: 90000 
     },
     {
         id: "m8",
         titulo: "Vestido",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m8.png",
+        imagen: "../img/Mujer/m8.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
         },
-        precio: 180.000 
+        precio: 180000 
     },
     {
         id: "m9",
         titulo: "Falda con short",
-        imagen: "C:\Users\Maria jose Horta\JMD SPORTS Ecommerce\img\Mujer\m9.png",
+        imagen: "../img/Mujer/m9.png",
         categoria: {
             nombre: "Mujer",
             id: "mujer"
@@ -283,18 +283,23 @@ const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
 const numerito = document.querySelector("#numerito");
 
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
 function cargarProductos(productosElegidos){
 
     contenedorProductos.innerHTML = "";
 
     productosElegidos.forEach(producto => {
+        
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}"></img>
             <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
-                <p class="producto-precio">${producto.precio}</p>
+                <p class="producto-precio">$${producto.precio}</p>
                 <button class="producto-agregar" id= "${producto.id}">Agregar</button>
             </div>
         `;
@@ -303,7 +308,7 @@ function cargarProductos(productosElegidos){
     actualizarBotonesAgregar();
 }
 
-cargarProductos(productos);
+// cargarProductos(productos);
 
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
