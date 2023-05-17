ad9e5e278c98c4d0d7f79001303310e8c1e82d15
majohorta04 <2220211028@estudiantesunibague.edu.co>
2023-05-16 22:25:59 -0500

    actualizacion main

--- js/main.js
+++ js/main.js
@@ -308,8 +308,6 @@ function cargarProductos(productosElegidos){
     actualizarBotonesAgregar();
 }
 
-cargarProductos(productos);
-
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
 
@@ -317,18 +315,18 @@ botonesCategorias.forEach(boton => {
         e.currentTarget.classList.add("active");
 
         if (e.currentTarget.id != "todos") {
-            const productosCategoria = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-            tituloPrincipal.innerText = e.currentTarget.innerText;
-          
-            cargarProductos(productosCategoria);
-          } else {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
             tituloPrincipal.innerText = "Todos los productos";
             cargarProductos(productos);
-          }        
+        }     
     })
 });
 
-function actualizarBotonesAgregar(){
+function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".producto-agregar");
 
     botonesAgregar.forEach(boton => {
@@ -342,16 +340,20 @@ function agregarAlCarrito(e){
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
 
-    if(productosEnCarrito.some(producto => producto.id === idBoton)){
-        productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
         productosEnCarrito[index].cantidad++;
-    }else{
+    } else {
         productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
     }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
 
-function actualizarNumerito(){
+function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
 }
